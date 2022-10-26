b5791ae6da19704a2e53cba9cc50e4c1f15aed92
binrui <bmu2@slb.com>
2022-10-26 13:38:21 +0800

    test: add the test for internal expense type given project is internal

--- ExpenseServiceTest/ExpenseServiceTest.cs
+++ ExpenseServiceTest/ExpenseServiceTest.cs
@@ -1,6 +1,7 @@
 using Expense.Service;
+using Expense.Service.Expense;
+using Expense.Service.Projects;
 using Xunit;
-
 namespace Expense.Service.Test
 {
     public class ExpenseServiceTest
@@ -9,8 +10,12 @@ namespace Expense.Service.Test
         public void Should_return_internal_expense_type_if_project_is_internal()
         {
             // given
+            Project project = new Project(ProjectType.INTERNAL, "internal_project");
+
             // when
+            ExpenseType expenseType = ExpenseService.GetExpenseCodeByProjectTypeAndName(project);
             // then
+            Assert.Equal(ExpenseType.INTERNAL_PROJECT_EXPENSE, expenseType);
         }
 
         [Fact]
